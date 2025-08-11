65b4f09039dab96ad7b8e06444c7060caa0c6b58
gualabanana <9ualabanana@gmail.com>
2025-08-11 19:56:24 +0700

    Stream dynamic dashboard content
    Split dashboard page into overview route with async components for cards, revenue chart, and latest invoices. Updated UI components to fetch their own data and handle empty states. Cleaned up unused code and enabled actual data fetching in query route. Minor cleanup in data fetching logic.

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -5,6 +5,7 @@ import {
   InboxIcon,
 } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import { fetchCardData } from '@/app/lib/data';
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -14,18 +15,26 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
+  const { totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers } = await fetchCardData();
+  if (
+    !totalPaidInvoices ||
+    !totalPendingInvoices ||
+    !numberOfInvoices ||
+    !numberOfCustomers
+  ) {
+    return <p className="mt-4 text-gray-400">No card data available.</p>;
+  }
+  
   return (
     <>
-      {/* NOTE: Uncomment this code in Chapter 9 */}
-
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      /> */}
+      />
     </>
   );
 }
